b4c6f1d00b335a34ee316e816fa0fc36549e9839
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-05-26 13:17:48 -0600

    Alphabet number positions in string without functions fail

--- CodingPracticeProblems.js
+++ CodingPracticeProblems.js
@@ -1,9 +1,49 @@
 /*Length of String*/
-function("Hello");
+/* document.getElementById("javaScript").innerHTML = stringLength("abc"); */ /* sending to html and calling function for string */
 
-function strLength(str){
-    let length = 0;
-    while (str[length]!=undefined)
-        length = length +1;
-    return length;
+function stringLength(input){
+    let strLength = 0;
+    while (input[strLength]!=undefined)
+        strLength++;
+    return strLength;
+    }
+
+
+/*Alphabet Position in String*/
+document.getElementById("javaScript").innerHTML = alphabetPosition("a b c");
+
+function alphabetPosition(input){
+    let abc ="abcdefghijklmnopqrstuvwxyz";
+    let inputLength = 0;
+
+    let output =[];
+    
+    input = input.toLowerCase();
+
+    while(input[inputLength]!=undefined){ 
+        inputLength ++;
+    }
+    
+        for(let inputCurrentPosition = 0; inputCurrentPosition < inputLength; inputCurrentPosition++){
+                
+            for(let i = 0; i < 26; i++){
+                if(input[inputCurrentPosition] === abc[i]){ 
+                    output[inputCurrentPosition] = i+1; /* adding one because abc char position begins at zero */
+                }   
+            }
+
+        }
+
+        for(let i = 0; i < inputLength; i++){
+            if(!output[i] > 0){
+                output[i] = "";
+            }
+        }
+
+
+    output = output.join(" ");
+    let string = output + "";
+
+    return string;
+
 }
