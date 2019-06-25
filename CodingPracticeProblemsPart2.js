de7300311aa21a4b07859a438dc3e07b2c37eecb
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-24 20:19:45 -0600

    Success! Sorting Even and Odds while maintaining position in array

--- CodingPracticeProblemsPart2.js
+++ CodingPracticeProblemsPart2.js
@@ -2,7 +2,7 @@
   //zero is even
   //ex: sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 8, 4, 5, 2]
   
-  document.getElementById("javaScript").innerHTML = sortEvensOddsArray([1,7,3,11,5,23]);
+  document.getElementById("javaScript").innerHTML = sortEvensOddsArray([1, 111, 11, 11, 2, 1, 5, 0]) +"   "+ "expect: 1, 1, 5, 11, 2, 11, 111, 0";
 
   function sortEvensOddsArray(array){
     
@@ -11,7 +11,7 @@
       
        for(let k=0; k<array.length;k++){
         //even number logic descending order
-        if(array[k]%2 === 0){
+        if(array[i]%2 === 0 && array[k]%2 === 0){
 
           if(array[i] > array[k]){
 
@@ -19,21 +19,20 @@
             let higherEvenNum = array[i];
             array[k] = higherEvenNum;
             array[i] = lowerEvenNum;
-            console.log("high " + higherEvenNum);
-            console.log("low " + lowerEvenNum);
-            console.log(array)
+         
           }
     
         }
         //off number logic ascending order
-        if(array[i]%2 !==0){
-          if(array[i] < array[k]){
+        if(array[i]%2 !==0 && array[k]%2 !==0){
+          
+            if(array[i] < array[k]){
+                
+                let lowerEvenNum = array[k];
+                let higherEvenNum = array[i];
+                array[k] = higherEvenNum;
+                array[i] = lowerEvenNum;
 
-            let lowerEvenNum = array[k];
-            let higherEvenNum = array[i];
-            array[k] = higherEvenNum;
-            array[i] = lowerEvenNum;
-      
           }
         
         }
