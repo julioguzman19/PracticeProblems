13cf8f5d6743ea022b48a28a272a0bd80b06abdb
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-02 15:09:43 -0600

    SUCCESS Logic for Convert stringToNumber

--- CodingPracticeProblemsPart2.js
+++ CodingPracticeProblemsPart2.js
@@ -271,11 +271,13 @@ function getSize(width,height,depth){
   //Conver a string to a number
   document.getElementById("javaScript").innerHTML =stringToNumber("123")
   function stringToNumber(str){
-    let number = 0;
+    let number = "";
 
     for(let i =0 ; i<str.length; i++){
       number = number +str.charAt(i);
+      console.log(number);
+   
     }
-    number = number.slice(0)
+
     return number;  
   }
