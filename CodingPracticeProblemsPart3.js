f134cedeaea173ef4f24cbf1cd92b5f71c9cf66e
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-16 14:03:33 -0600

    initializing isInteresting number no,almost,yes based on criteria

--- CodingPracticeProblemsPart3.js
+++ CodingPracticeProblemsPart3.js
@@ -1,4 +1,4 @@
-document.getElementById("javaScript").innerHTML = adjacentElementsProduct([1,2,3]);
+/* document.getElementById("javaScript").innerHTML = adjacentElementsProduct([1,2,3]); */
 
 function adjacentElementsProduct(array) {
     let result = null;
@@ -9,3 +9,20 @@ function adjacentElementsProduct(array) {
     }
     return result;
 }
+
+//Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+//Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+/* 
+-Any digit followed by all zeros: 100, 90000
+-Every digit is the same number: 1111
+-The digits are sequential, incementing†: 1234
+-The digits are sequential, decrementing‡: 4321
+-The digits are a palindrome: 1221 or 73837
+-The digits match one of the values in the awesomePhrases array 
+*/
+
+document.getElementById("javaScript").innerHTML = isInteresting(3, [1337, 256]);
+
+function isInteresting(number, awesomePhrases) {
+   
+  }
