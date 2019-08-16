1824f53cfee744d31d9fa436ee1bd66544e48ba7
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-15 21:17:25 -0600

    flattenAndSort arrays within array failing

--- CodingPracticeProblemsPart4.js
+++ CodingPracticeProblemsPart4.js
@@ -4,9 +4,29 @@ Test.assertSimilar(flattenAndSort([[], [1]]), [1]);
 Test.assertSimilar(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 Test.assertSimilar(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
 */
-document.getElementById("javaScript").innerHTML = flattenAndSort([]);
+document.getElementById("javaScript").innerHTML = flattenAndSort([[],[5],[2,3]]);
 
 function flattenAndSort(array) {
-    // Good luck, brave code warrior!
-    return array;
+    
+    let result = [];
+    
+
+    for(let i=0; i<array.length; i++){
+        result.push(array[i]);
+    }
+
+    for(let i=0; i<array.length; i++){
+        for(let k=0; k<array.length; k++){
+            console.log(result[i]);
+            if(result[i] > result[k]){
+                min = result[k];
+                result[k] = result[i];
+                result[i] = min;
+                console.log("min: " + min + " result: " + result);
+                
+            }
+        }
+    }
+
+    return result;
   }
