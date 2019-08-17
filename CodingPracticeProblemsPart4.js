501dba3337fb9d5a069cc408aa8e6f92c06e3b6b
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-16 20:52:03 -0600

    success

--- CodingPracticeProblemsPart4.js
+++ CodingPracticeProblemsPart4.js
@@ -4,29 +4,41 @@ Test.assertSimilar(flattenAndSort([[], [1]]), [1]);
 Test.assertSimilar(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 Test.assertSimilar(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
 */
-document.getElementById("javaScript").innerHTML = flattenAndSort([[],[5],[2,3]]);
+document.getElementById("javaScript").innerHTML = flattenAndSort([[2,3,4],[1,1,8,3]]);
 
 function flattenAndSort(array) {
     
     let result = [];
     
-
-    for(let i=0; i<array.length; i++){
-        result.push(array[i]);
+    if(array.length ===0){
+        return array;
     }
 
-    for(let i=0; i<array.length; i++){
-        for(let k=0; k<array.length; k++){
-            console.log(result[i]);
-            if(result[i] > result[k]){
-                min = result[k];
-                result[k] = result[i];
-                result[i] = min;
-                console.log("min: " + min + " result: " + result);
-                
+
+    //putting object into array
+    for(let i=0;i<array.length;i++){
+            let j=0;
+            while(array[i][j]!==undefined){
+                result.push(array[i][j])
+                j++;
             }
+            
+            console.log(result)
         }
-    }
+    
+
+    
+     for(let i=0; i<result.length; i++){
+
+        for(let k=0; k<result.length; k++){
+            
+                if(result[i] < result[k]){
+                    let max = result[k];
+                    result[k] = result[i];
+                    result[i] = max;
+                }
+        }
+    } 
 
     return result;
   }
