293f6918028498bbc0109e44d9aa81d02377b1d2
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-30 18:53:57 -0600

    initializing productArray

--- CodingPracticeProblemsPart4.js
+++ CodingPracticeProblemsPart4.js
@@ -279,3 +279,16 @@ String.prototype.isUpperCase = function() {
     }
     return result;
   }
+
+
+ /*  
+  Test.assertSimilar(productArray([12,20]), [20,12]);
+  Test.assertSimilar(productArray([12,20]), [20,12]);
+  Test.assertSimilar(productArray([3,27,4,2]), [216,24,162,324]);
+  Test.assertSimilar(productArray([13,10,5,2,9]), [900,1170,2340,5850,1300]);
+  Test.assertSimilar(productArray([16,17,4,3,5,2]), [2040,1920,8160,10880,6528,16320]); */
+
+  document.getElementById("javaScript").innerHTML = productArray([12,20])
+  function productArray(numbers){
+    //your code here
+  }
