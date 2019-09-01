347f29cd96affff4757a09801d95d393e084e898
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-31 18:44:13 -0600

    encodingString success

--- CodingPracticeProblemsPart5.js
+++ CodingPracticeProblemsPart5.js
@@ -2,24 +2,22 @@
 //aabbbc >>> a2b3c1
 //aaabcc >>> a3b1c2
 
-document.getElementById("javaScript").innerHTML = encodingString("aabbc");
+document.getElementById("javaScript").innerHTML = encodingString("aabbbc");
 
 function encodingString(str){
     let result = "";
-    let count = 1;
+    let count = 0;
 
     for(let i=0;i<str.length;i++){
         if(str.charAt(i) === str.charAt(i+1)){
-            console.log("yee");
             count++;
         }
         else if(str.charAt(i) !== str.charAt(i+1) && str.charAt(i+1)!== undefined){
+            count++;
             result = result + str.charAt(i) + count;
             count = 0;
         }
-        else{
-            result = result +str.charAt(i)
-        }
+
     }
     return result;
 }
